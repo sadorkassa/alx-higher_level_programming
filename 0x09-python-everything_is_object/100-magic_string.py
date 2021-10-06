a013bb3db3c44003a72f0976df2621a8357c3da5
Sador Kassa <sadorkassa21@gmail.com>
2021-10-06 07:49:50 +0000

    0x09. Python - Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
-def call_counter(func):
-    def helper():
-        helper.calls += 1
-        return func()
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
-def magic_string():
-    return 'Holberton, ' * (magic_string.calls - 1) + 'Holberton'
+def magic_string(to=[0]):
+    to[0] += 1
+    return ('Holberton, ' * (to[0] - 1)) + 'Holberton'
