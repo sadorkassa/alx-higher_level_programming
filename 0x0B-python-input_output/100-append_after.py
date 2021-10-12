5280269ed540417fb47d5a095d9dfb7e442c102b
Sador Kassa <sadorkassa21@gmail.com>
2021-10-12 12:07:07 +0000

    0x0B-python-input_output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""append after module"""
+"""Module for append_after"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """append a text after a substring line"""
+    """Inserts a line of text to a file, after each line
+       containing a specific string
+    """
+    output = ''
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            output += line
+            if search_string in line:
+                output += new_string
 
-    with open(filename, 'r') as f:
-        content = f.readlines()
-        for (index, line) in enumerate(content):
-            if line.find(search_string) != -1:
-                content.insert(index+1, new_string)
-        new_content = "".join(content)
-    f = open(filename, 'w')
-    f.write(new_content)
-    f.close()
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(output)
