345668164f0dec4967f54e29406bcc5f1e98c8fa
Sador Kassa <sadorkassa21@gmail.com>
2021-12-13 14:52:21 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-// Adding a new function incr that increments the integer value
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () { this.value++; };
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
