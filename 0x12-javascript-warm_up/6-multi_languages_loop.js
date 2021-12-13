da1d41101c9ecb4bf52ea3b030539382175be04e
Sador Kassa <sadorkassa21@gmail.com>
2021-12-13 14:46:07 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-// iterate list
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let step = 0; step < array.length; step++) {
-  console.log(array[step]);
+// Uses an array to loop through and print strings
+
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
 }
