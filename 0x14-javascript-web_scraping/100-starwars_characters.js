0761284b063d5c25ffe36ae01b686caa37888d7c
Sador Kassa <sadorkassa21@gmail.com>
2022-02-01 13:36:56 +0000

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,25 +1,28 @@
 #!/usr/bin/node
-const request = require('request');
-
-const episodeId = process.argv[2];
-
-if (parseInt(episodeId) < 8) {
-  const url = 'https://swapi-api.hbtn.io/api/films/' + episodeId;
-
-  request(url, (err, res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    const { characters } = JSON.parse(body);
-
-    characters.forEach(
-      (url, index) => {
-        request(url, (err, res, body) => {
-          if (err) {
-            return console.log(err);
+let request = require('request');
+let episodeNumber = process.argv[2];
+let url = 'http://swapi.co/api/films/' + episodeNumber;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let charDict = {};
+    let allChars = JSON.parse(body).characters;
+    for (let c in allChars) {
+      request(allChars[c], function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          charDict[c] = JSON.parse(body).name;
+        }
+        if (allChars.length === Object.keys(charDict).length) {
+          for (let key in charDict) {
+            console.log(charDict[key]);
           }
-          console.log(JSON.parse(body).name);
-        });
+        }
       });
-  });
-}
+    }
+  } else {
+    console.log('Wrong status code');
+  }
+});
